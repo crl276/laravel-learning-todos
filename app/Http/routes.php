adabbdc79f1a4576e5dcbbb8173d0aa2dd735bb7
Callan Lamb <callan.lamb@gmail.com>
2017-03-15 21:09:23 -0400

    Add routes for TaskController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
