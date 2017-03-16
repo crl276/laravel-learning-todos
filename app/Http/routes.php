ff63a35cdd845289b736dd2615018a7fc661083e
Callan Lamb <callan.lamb@gmail.com>
2017-03-15 21:06:07 -0400

    Add register routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
