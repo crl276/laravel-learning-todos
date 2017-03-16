d601f516ef24914d3c5a5415517a2f04d089f72c
Callan Lamb <callan.lamb@gmail.com>
2017-03-15 21:25:15 -0400

    Add base layout in app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Daily Meditation</title>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
